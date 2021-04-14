daab5daa454f1889ef5a0eb4c5948526970eb71f
redraskal <6241454+redraskal@users.noreply.github.com>
2021-04-14 02:46:09 -0500

    Elixir protobuf types

--- server/mix.exs
+++ server/mix.exs
@@ -25,7 +25,8 @@ defmodule NotUnoServer.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:riverside, "~> 1.2.6"},
-      {:protobuf, git: "https://github.com/elixir-protobuf/protobuf.git", branch: "master"}
+      {:jason, "~> 1.2"},
+      {:protox, "~> 1.3.2"}
     ]
   end
 end
